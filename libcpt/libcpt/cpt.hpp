8722234e93f9654c32ff21533e4b544920ea6643
btmxh <ngoduyanh.chip@gmail.com>
2024-06-19 15:21:42 +0700

    feat: add a main header file

---
+++ libcpt/libcpt/cpt.hpp
@@ -0,0 +1,9 @@
+#pragma cpt begin
+#include "assert.hpp"
+#include "input.hpp"
+#include "is_iterable.hpp"
+#include "iterate.hpp"
+#include "mod.hpp"
+#include "print.hpp"
+#include "types.hpp"
+#pragma cpt end
