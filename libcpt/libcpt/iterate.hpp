b0e8fc501d08a9fc5df5d952ca503f4888be7139
btmxh <ngoduyanh.chip@gmail.com>
2024-06-19 15:59:25 +0700

    fix: only decompose if needed in `fe` macro

--- libcpt/libcpt/iterate.hpp
+++ libcpt/libcpt/iterate.hpp
@@ -14,7 +14,7 @@
 #define fs(i, s, e, st) for (::cpt::ll i = s; i < e; i += st)
 #define rf(i, s, e) rfs(i, s, e, 1)
 #define rfs(i, s, e, st) for (::cpt::ll i = e - 1; i >= s; i -= st)
-#define fe(c, ...) for (auto &[__VA_ARGS__] : c)
+#define fe(c, ...) for (auto &__VA_ARGS__ : c)
 #define fer(itr, c) for (auto itr = c.rbegin(); itr != c.rend(); ++itr)
 #define fdiv(x, n)                                                             \
   for (::cpt::ll x = 1; x * x <= n; ++x)                                       \
