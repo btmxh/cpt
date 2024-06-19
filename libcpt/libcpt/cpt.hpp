4cef30f469f8bdf2447fa3b1f5294d81f87be3c7
btmxh <ngoduyanh.chip@gmail.com>
2024-06-19 17:42:28 +0700

    feat: add DSU library

--- libcpt/libcpt/cpt.hpp
+++ libcpt/libcpt/cpt.hpp
@@ -1,6 +1,7 @@
 #pragma cpt begin
 #include "assert.hpp"
 #include "algorithm.hpp"
+#include "dsu.hpp"
 #include "input.hpp"
 #include "is_iterable.hpp"
 #include "iterate.hpp"
---
+++ libcpt/libcpt/dsu.hpp
@@ -0,0 +1,82 @@
+#pragma once
+
+#include "types.hpp"
+namespace cpt {
+class naive_disjoint_set {
+  vll parent;
+
+public:
+  naive_disjoint_set(ll n) : parent(n) {}
+
+  void make_set(ll x) { parent[x] = x; }
+  ll repr(ll x) {
+    if (x == parent[x]) {
+      return x;
+    }
+    return parent[x] = repr(parent[x]);
+  }
+  bool unite(ll a, ll b) {
+    a = repr(a);
+    b = repr(b);
+    if (a == b) {
+      return false;
+    }
+
+    merge(a, b);
+    return true;
+  }
+
+protected:
+  void merge(ll a, ll b) { parent[a] = b; }
+};
+
+class sized_disjoint_set : public naive_disjoint_set {
+private:
+  vll size;
+
+public:
+  sized_disjoint_set(ll n) : naive_disjoint_set(n), size(n) {}
+
+  void make_set(ll x) {
+    naive_disjoint_set::make_set(x);
+    size[x] = 1;
+  }
+
+  ll get_size(ll a) { return size[repr(a)]; }
+
+protected:
+  void merge(ll a, ll b) {
+    if (size[a] > size[b]) {
+      std::swap(a, b);
+    }
+
+    naive_disjoint_set::merge(a, b);
+    size[b] += size[a];
+  }
+};
+
+class ranked_disjoint_set : public naive_disjoint_set {
+private:
+  vll rank;
+
+public:
+  ranked_disjoint_set(ll n) : naive_disjoint_set(n), rank(n) {}
+
+  void make_set(ll x) {
+    naive_disjoint_set::make_set(x);
+    rank[x] = 0;
+  }
+
+  ll get_rank(ll a) { return rank[repr(a)]; }
+
+protected:
+  void merge(ll a, ll b) {
+    if (rank[a] > rank[b])
+      std::swap(a, b);
+    naive_disjoint_set::merge(a, b);
+    rank[b] += rank[a] == rank[b];
+  }
+};
+} // namespace cpt
+
+using dsu = cpt::sized_disjoint_set;
