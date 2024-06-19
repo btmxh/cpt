c72b230e33a21dcb871bf4434b6ef6196ce50124
btmxh <ngoduyanh.chip@gmail.com>
2024-06-19 11:09:25 +0700

    feat: intitr and binary search functions

--- cpt/cpp17/iterate.hpp
+++ cpt/cpp17/iterate.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include <type_traits>
+#include <utility>
+
 #define f(i, s, e) fs(i, s, e, 1)
 #define fs(i, s, e, st) for (::cpt::ll i = s; i < e; i += st)
 #define rf(i, s, e) rfs(i, s, e, 1)
@@ -13,3 +19,66 @@
 #define all(v) ::std::begin(v), ::std::end(v)
 #define allc(v) ::std::cbegin(v), ::std::cend(v)
 #define allr(v) ::std::rbegin(v), ::std::rend(v)
+
+namespace cpt {
+template <class Int> class int_iterator {
+private:
+  Int value;
+
+public:
+  using difference_type = std::make_signed_t<Int>;
+  int_iterator(Int value) : value{value} {}
+
+  operator Int() const { return value; }
+  Int operator*() const { return value; }
+
+  Int operator++() { return ++value; }
+  Int operator++(int) { return value++; }
+  Int operator--() { return --value; }
+  Int operator--(int) { return value--; }
+  Int operator+=(Int amt) { return value += amt; }
+  Int operator-=(Int amt) { return value -= amt; }
+  friend difference_type operator-(Int x, Int y) { return x.value - y.value; }
+  friend difference_type operator+(Int x, Int y) { return x.value + y.value; }
+
+  bool operator==(const Int &other) const = default;
+  bool operator!=(const Int &other) const = default;
+  bool operator<(const Int &other) const = default;
+  bool operator>(const Int &other) const = default;
+  bool operator<=(const Int &other) const = default;
+  bool operator>=(const Int &other) const = default;
+};
+} // namespace cpt
+
+namespace std {
+template <class Int> struct std::iterator_traits<::cpt::int_iterator<Int>> {
+  using difference_type = std::make_signed_t<Int>;
+  using value_type = Int;
+  using iterator_category = std::random_access_iterator_tag;
+};
+} // namespace std
+
+#ifdef __cpp_lib_span
+#include <span>
+
+template <class T> inline auto bisect_span(span<T> sp) {
+  size_t mid = sp.size() / 2;
+  auto left = sp.subspan(0, mid);
+  auto right = sp.subspan(mid, sp.size() - mid);
+  return std::make_pair(left, right);
+}
+#endif
+
+// binary search helpers
+template <class It, class Func> It first_true(It begin, It end, Func func) {
+  return std::upper_bound(begin, end, nullptr,
+                          [func](auto, auto x) { return func(x); });
+}
+
+template <class It, class Func> It last_true(It begin, It end, Func func) {
+  It first_false = first_true(begin, end, std::not_fn(func));
+  if (first_false == end) {
+    return end;
+  }
+  return --first_false;
+}
